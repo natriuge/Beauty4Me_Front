8849ba180331846dd4e48339f9c2606dc5fb60f1
Nathalia Maia <nathaliamaiape@gmail.com>
2022-06-14 23:16:27 -0300

    initial structure of product details

--- src/apis/api.js
+++ src/apis/api.js
@@ -1,18 +1,25 @@
 import axios from "axios";
 
-const apis = {
-  development: "A URL DO SEU SERVIDOR EXPRESS LOCAL AQUI",
-  production: "A URL DO SEU SERVIDOR DEPLOYADO NO HEROKU AQUI",
-};
+// const apis = {
+//   development: "http://localhost:4000",
+//   production: "A URL DO SEU SERVIDOR DEPLOYADO NO HEROKU AQUI",
+// };
 
 // Pré-configurando a URL padrão do nosso backend em uma instância do Axios
+// const api = axios.create({
+//   baseURL: apis[process.env.NODE_ENV],
+// });
+
+//apagar este depois que tivermos a url do deploy e usar o acima
 const api = axios.create({
-  baseURL: apis[process.env.NODE_ENV],
+  baseURL: "http://localhost:5000",
 });
 
 // Configura a instância do Axios para injetar o cabeçalho de autenticação antes de cada requisição
+//intercepta ANTES de toda a requisição nesta instância específica que configuramos
 api.interceptors.request.use((config) => {
   // Verifica se já temos as informações do usuário logado no localStorage
+  //localstorage: sobrevive em um arquivo no pc do usuário até que o usuário manualmente delete do pc dele ou formate o pc dele. o token expira, mas enquanto ele for válido, o usuário fica logado!
   const storedUser = localStorage.getItem("loggedInUser");
 
   const loggedInUser = JSON.parse(storedUser || '""');
--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,7 @@ import Login from "../pages/auth/Login";
 import ProtectedRoute from "../pages/auth/ProtectedRoute";
 
 import { AuthContextComponent } from "../contexts/authContext";
+import ProductDetails from "./ProductDetails";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/" element={<ProtectedRoute component={Home} />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/product-detail" element={<ProductDetails />} />
       </Routes>
     </AuthContextComponent>
   );
