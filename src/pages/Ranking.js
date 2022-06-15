09a194b26bd1888ffa0e04b73ff578a663e034b5
Camila Solér <msoler.camila@gmail.com>
2022-06-15 12:31:11 -0300

    início raking page

---
+++ src/pages/Ranking.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import api from "../../apis/api";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../../assets/styles/loginSignupStyle.css"; //fazer um pra ranking
+import Navbar from "../../components/Navbar";
+
+function Ranking() {
+  <div>
+    <Navbar />
+    <p>oi</p>
+  </div>;
+}
+
+export default Ranking;
