45a902ded27e1424eb67be80825053e817d6af06
Camila Solér <msoler.camila@gmail.com>
2022-06-15 21:43:39 -0300

    atualizando a page ranking

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -29,8 +29,10 @@ function Ranking() {
       <Navbar />
 
       <div className="mb-5">
-        <h1>Ranking Page</h1>
-        <h4>Check out the best products by customers review</h4>
+        <h1 className="h1-title">Ranking Page</h1>
+        <h4 className="h4-title">
+          Check out the best products by customers review
+        </h4>
       </div>
 
       <div
@@ -50,92 +52,20 @@ function Ranking() {
                     alt={productName}
                   />
                   <div className="card-middle-ranking">
-                    <div className="card-text-ranking">Go to product</div>
+                    <button className="btn card-text-ranking">
+                      <i className="bi bi-search"></i>
+                    </button>
                   </div>
                 </div>
-
                 <div className="card-body">
-                  <h5 className="card-title">{productName}</h5>
-                  <p className="card-text">{brandName}</p>
-                  <p className="card-text">{rating}</p>
-                </div>
-                <div>
-                  <button
-                    className="btn col" //fazer css do btn
-                    type=""
-                    //value={} qual é?
-                    //onClick={} ir para detail do produto
-                  >
-                    {" "}
-                    Check it out!
-                  </button>
+                  <h6 className="card-title h6-name">{productName}</h6>
+                  <p className="card-text p-brand-name">{brandName}</p>
+                  <p className="card-text p-ranting">{rating}</p>
                 </div>
               </div>
             </div>
           );
         })}
-        <div className="col">
-          <div className="card h-100">
-            <img src="..." className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">
-                This is a longer card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="col">
-          <div className="card h-100">
-            <img src="..." className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">This is a short card.</p>
-            </div>
-          </div>
-        </div>
-        <div className="col">
-          <div className="card h-100">
-            <img src="..." className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">
-                This is a longer card with supporting text below as a natural
-                lead-in to additional content.
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="col">
-          <div className="card h-100">
-            <img src="..." className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">
-                This is a longer card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="col">
-          <div className="card h-100">
-            <img src="..." className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">
-                This is a longer card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer. This is a longer card with supporting text below as a
-                natural lead-in to additional content. This content is a little
-                bit longer.
-              </p>
-            </div>
-          </div>
-        </div>
       </div>
     </div>
   );
